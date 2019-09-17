d1c6b1d915bf7da62d3f895a93009ae3a04d4be5
Matias Figueredo <mfigueredo@aconcaguasf.com>
2019-09-17 16:03:52 -0300

    Cambio de mensaje

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express(); // creo una aplicacion Express
 
 app.get('/', (req, res) => {
-    res.send({ hola: 'vo'});
+    res.send({ chau: 'chango' });
 });
 
 const PORT = process.env.PORT || 5000;
